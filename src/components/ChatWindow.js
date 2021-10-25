569cd42ba2b4acf94619464612c6e1476848752f
Rahul Rayaprolu <sameera.12.kasibhatta@gmail.com>
2021-10-25 23:15:36 +0530

    Updated UI

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ChatWindow.css';
-//import {MdRiSendPlaneLine} from "react-icons/md";
 import { RiSendPlaneFill } from "react-icons/ri";
+
 const ChatWindow = () => {
+    const [text, setText] = useState('');
+    const [message, setMessage] = useState([]);
+
+    const onButtonClick = () => {     
+        setMessage(msg => [...msg, text]); 
+        console.log(message)
+        setText('');
+    }
+
+    const getMessages = () => {
+       return message.map(msg => msg && <div key={`${Math.random()*1000+1}`}>{msg}</div>)
+    }
+
     return(
         <div className='main-container'>
             <div className='container-header'>
                 Web Sockets with Spring Boot Chat
             </div>
             <div className="container-body">
+                { getMessages()}
             </div>
             <div className='container-footer'>
                 <div className='footer-inner-container'>
-                    <textarea id='text' rows='2' placeholder='Type your message...'></textarea>
-                   <RiSendPlaneFill id='send' size='30px'/>
+                    <textarea id='text' rows='2' 
+                        value={text}
+                        onChange={e=>setText(e.target.value)}
+                        placeholder='Type your message...'></textarea>
+                    <RiSendPlaneFill id='send'
+                    onClick = { onButtonClick}
+                      size='30px'/>
                 </div>
             </div>
         </div>
